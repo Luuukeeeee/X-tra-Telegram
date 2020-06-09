d41905eba98e418492f4957bee7219d17d438d00
Luuukeeeee <65567900+Luuukeeeee@users.noreply.github.com>
2020-06-09 22:53:11 +0200

    Update 4_5965070732118460159.py

--- userbot/plugins/4_5965070732118460159.py
+++ userbot/plugins/4_5965070732118460159.py
@@ -24,3 +24,29 @@ async def _(event):
         await event.reply(mentions)
     await event.delete()
 
+@borg.on(admin_cmd("steve"))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+    mentions = " 🏿🏿🏿🏿🏿🏿🏿🏿
+🏿🏿🏿🏿🏿🏿🏿🏿
+🏿🏿🏽🏽🏽🏽🏿🏿
+🏽🏽🏽🏽🏽🏽🏽🏽
+🏽⬜⬛🏽🏽⬛⬜🏽
+🏽🏽🏽🏿🏿🏽🏽🏽
+🏽🏽🏿🏽🏽🏿🏽🏽
+🏽🏽🏿🏿🏿🏿🏽🏽 "
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
